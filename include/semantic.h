e2d632b4a740f5510b6bd8decf64284f612b5da2
octalide <ethayng@gmail.com>
2025-01-01 17:27:28 -0500

    repository cleanup. switch to C23

--- include/semantic.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEMANTIC_H
-#define SEMANTIC_H
-
-#include "ast.h"
-#include "symbols.h"
-#include "project.h"
-
-#endif // SEMANTIC_H
--- src/semantic.c
+++
@@ -1,2 +0,0 @@
-#include "semantic.h"
-#include <stdio.h>

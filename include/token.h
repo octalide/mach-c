33796fb9e7374365793e8d725f41aad6606fec01
octalide <ethayng@gmail.com>
2025-06-26 00:03:22 -0400

    refactor: enhance token handling by reorganizing token kinds and updating initialization functions

--- include/token.h
+++ include/token.h
@@ -3,31 +3,63 @@
 
 typedef enum TokenKind
 {
-    TOKEN_LIST_ERROR = -2,
-    TOKEN_ERROR,
-
-    TOKEN_EOF,
+    TOKEN_ERROR = -1,
+    TOKEN_EOF = 0,
     TOKEN_COMMENT,
 
-    TOKEN_IDENTIFIER,
-
+    // literals
     TOKEN_LIT_INT,
     TOKEN_LIT_FLOAT,
     TOKEN_LIT_CHAR,
     TOKEN_LIT_STRING,
 
-    TOKEN_L_PAREN,   // (
-    TOKEN_R_PAREN,   // )
-    TOKEN_L_BRACKET, // [
-    TOKEN_R_BRACKET, // ]
-    TOKEN_L_BRACE,   // {
-    TOKEN_R_BRACE,   // }
-    TOKEN_COLON,     // :
-    TOKEN_SEMICOLON, // ;
-    TOKEN_QUESTION,  // ?
-    TOKEN_AT,        // @
-    TOKEN_DOT,       // .
-    TOKEN_COMMA,     // ,
+    TOKEN_IDENTIFIER,
+
+    // keywords
+    TOKEN_KW_USE, // use
+    TOKEN_KW_EXT, // ext
+    TOKEN_KW_DEF, // def
+    TOKEN_KW_STR, // str
+    TOKEN_KW_UNI, // uni
+    TOKEN_KW_VAL, // val
+    TOKEN_KW_VAR, // var
+    TOKEN_KW_FUN, // fun
+    TOKEN_KW_RET, // ret
+    TOKEN_KW_IF,  // if
+    TOKEN_KW_OR,  // or
+    TOKEN_KW_FOR, // for
+    TOKEN_KW_CNT, // cnt
+    TOKEN_KW_BRK, // brk
+
+    // value keywords
+    TOKEN_TYPE_NAT, // "not a type" (`void` equivalent)
+    TOKEN_TYPE_I8,  // i8
+    TOKEN_TYPE_I16, // i16
+    TOKEN_TYPE_I32, // i32
+    TOKEN_TYPE_I64, // i64
+    TOKEN_TYPE_U8,  // u8
+    TOKEN_TYPE_U16, // u16
+    TOKEN_TYPE_U32, // u32
+    TOKEN_TYPE_U64, // u64
+    TOKEN_TYPE_F32, // f32
+    TOKEN_TYPE_F64, // f64
+
+    // punctuation
+    TOKEN_L_PAREN,    // (
+    TOKEN_R_PAREN,    // )
+    TOKEN_L_BRACKET,  // [
+    TOKEN_R_BRACKET,  // ]
+    TOKEN_L_BRACE,    // {
+    TOKEN_R_BRACE,    // }
+    TOKEN_COLON,      // :
+    TOKEN_SEMICOLON,  // ;
+    TOKEN_QUESTION,   // ?
+    TOKEN_AT,         // @
+    TOKEN_DOT,        // .
+    TOKEN_COMMA,      // ,
+    TOKEN_UNDERSCORE, // _
+
+    // operators - single character
     TOKEN_PLUS,      // +
     TOKEN_MINUS,     // -
     TOKEN_STAR,      // *
@@ -41,7 +73,8 @@ typedef enum TokenKind
     TOKEN_EQUAL,     // =
     TOKEN_BANG,      // !
     TOKEN_SLASH,     // /
-    TOKEN_BACKSLASH,
+
+    // operators - multi-character
     TOKEN_EQUAL_EQUAL,         // ==
     TOKEN_BANG_EQUAL,          // !=
     TOKEN_LESS_EQUAL,          // <=
@@ -60,11 +93,10 @@ typedef struct Token
     int len;
 } Token;
 
-Token *token_new(TokenKind kind, int pos, int len);
-void token_free(Token *token);
+void token_init(Token *token, TokenKind kind, int pos, int len);
+void token_dnit(Token *token);
 
-Token *token_copy(Token *token);
-
+void token_copy(Token *src, Token *dst);
 char *token_kind_to_string(TokenKind kind);
 
 #endif
--- src/token.c
+++ src/token.c
@@ -2,40 +2,43 @@
 
 #include <stdlib.h>
 
-Token *token_new(TokenKind kind, int pos, int len)
+void token_init(Token *token, TokenKind kind, int pos, int len)
 {
-    Token *token = calloc(1, sizeof(Token));
     token->kind = kind;
     token->pos = pos;
     token->len = len;
-
-    return token;
 }
 
-void token_free(Token *token)
+void token_dnit(Token *token)
 {
-    free(token);
+    token->kind = TOKEN_ERROR;
+    token->pos = -1;
+    token->len = -1;
 }
 
-Token *token_copy(Token *token)
+void token_copy(Token *src, Token *dst)
 {
-    return token_new(token->kind, token->pos, token->len);
+    if (src == NULL || dst == NULL)
+    {
+        return;
+    }
+
+    dst->kind = src->kind;
+    dst->pos = src->pos;
+    dst->len = src->len;
 }
 
 char *token_kind_to_string(TokenKind kind)
 {
     switch (kind)
     {
-    case TOKEN_LIST_ERROR:
-        return "LIST_ERROR";
     case TOKEN_ERROR:
         return "ERROR";
     case TOKEN_EOF:
         return "EOF";
     case TOKEN_COMMENT:
         return "COMMENT";
-    case TOKEN_IDENTIFIER:
-        return "IDENTIFIER";
+
     case TOKEN_LIT_INT:
         return "LIT_INT";
     case TOKEN_LIT_FLOAT:
@@ -44,77 +47,136 @@ char *token_kind_to_string(TokenKind kind)
         return "LIT_CHAR";
     case TOKEN_LIT_STRING:
         return "LIT_STRING";
+
+    case TOKEN_IDENTIFIER:
+        return "IDENTIFIER";
+
+    case TOKEN_KW_USE:
+        return "use";
+    case TOKEN_KW_EXT:
+        return "ext";
+    case TOKEN_KW_DEF:
+        return "def";
+    case TOKEN_KW_STR:
+        return "str";
+    case TOKEN_KW_UNI:
+        return "uni";
+    case TOKEN_KW_VAL:
+        return "val";
+    case TOKEN_KW_VAR:
+        return "var";
+    case TOKEN_KW_FUN:
+        return "fun";
+    case TOKEN_KW_RET:
+        return "ret";
+    case TOKEN_KW_IF:
+        return "if";
+    case TOKEN_KW_OR:
+        return "or";
+    case TOKEN_KW_FOR:
+        return "for";
+    case TOKEN_KW_CNT:
+        return "cnt";
+    case TOKEN_KW_BRK:
+        return "brk";
+
+    case TOKEN_TYPE_NAT:
+        return "nat";
+    case TOKEN_TYPE_I8:
+        return "i8";
+    case TOKEN_TYPE_I16:
+        return "i16";
+    case TOKEN_TYPE_I32:
+        return "i32";
+    case TOKEN_TYPE_I64:
+        return "i64";
+    case TOKEN_TYPE_U8:
+        return "u8";
+    case TOKEN_TYPE_U16:
+        return "u16";
+    case TOKEN_TYPE_U32:
+        return "u32";
+    case TOKEN_TYPE_U64:
+        return "u64";
+    case TOKEN_TYPE_F32:
+        return "f32";
+    case TOKEN_TYPE_F64:
+        return "f64";
+
     case TOKEN_L_PAREN:
-        return "L_PAREN";
+        return "(";
     case TOKEN_R_PAREN:
-        return "R_PAREN";
+        return ")";
     case TOKEN_L_BRACKET:
-        return "L_BRACKET";
+        return "[";
     case TOKEN_R_BRACKET:
-        return "R_BRACKET";
+        return "]";
     case TOKEN_L_BRACE:
-        return "L_BRACE";
+        return "{";
     case TOKEN_R_BRACE:
-        return "R_BRACE";
+        return "}";
     case TOKEN_COLON:
-        return "COLON";
+        return ":";
     case TOKEN_SEMICOLON:
-        return "SEMICOLON";
+        return ";";
     case TOKEN_QUESTION:
-        return "QUESTION";
+        return "?";
     case TOKEN_AT:
-        return "AT";
+        return "@";
     case TOKEN_DOT:
-        return "DOT";
+        return ".";
     case TOKEN_COMMA:
-        return "COMMA";
+        return ",";
+    case TOKEN_UNDERSCORE:
+        return "_";
+
     case TOKEN_PLUS:
-        return "PLUS";
+        return "+";
     case TOKEN_MINUS:
-        return "MINUS";
+        return "-";
     case TOKEN_STAR:
-        return "STAR";
+        return "*";
     case TOKEN_PERCENT:
-        return "PERCENT";
+        return "%";
     case TOKEN_CARET:
-        return "CARET";
+        return "^";
     case TOKEN_AMPERSAND:
-        return "AMPERSAND";
+        return "&";
     case TOKEN_PIPE:
-        return "PIPE";
+        return "|";
     case TOKEN_TILDE:
-        return "TILDE";
+        return "~";
     case TOKEN_LESS:
-        return "LESS";
+        return "<";
     case TOKEN_GREATER:
-        return "GREATER";
+        return ">";
     case TOKEN_EQUAL:
-        return "EQUAL";
+        return "=";
     case TOKEN_BANG:
-        return "BANG";
+        return "!";
     case TOKEN_SLASH:
-        return "SLASH";
-    case TOKEN_BACKSLASH:
-        return "BACKSLASH";
+        return "/";
+
     case TOKEN_EQUAL_EQUAL:
-        return "EQUAL_EQUAL";
+        return "==";
     case TOKEN_BANG_EQUAL:
-        return "BANG_EQUAL";
+        return "!=";
     case TOKEN_LESS_EQUAL:
-        return "LESS_EQUAL";
+        return "<=";
     case TOKEN_GREATER_EQUAL:
-        return "GREATER_EQUAL";
+        return ">=";
     case TOKEN_LESS_LESS:
-        return "LESS_LESS";
+        return "<<";
     case TOKEN_GREATER_GREATER:
-        return "GREATER_GREATER";
+        return ">>";
     case TOKEN_AMPERSAND_AMPERSAND:
-        return "AMPERSAND_AMPERSAND";
+        return "&&";
     case TOKEN_PIPE_PIPE:
-        return "PIPE_PIPE";
+        return "||";
     case TOKEN_COLON_COLON:
-        return "COLON_COLON";
+        return "::";
+
     default:
-        return "INVALID";
+        return "UNKNOWN";
     }
 }
